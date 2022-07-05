a44decb6ecb5846dbfd53427b83a404b07f55f3c
alexangria1992 <alex040892@gmail.com>
2022-07-06 10:44:06 +1200

    Horizontal Scroll Bar setup with props as state

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box } from "@mui/material";
 import HeroBanner from "../components/HeroBanner";
 import SearchExercises from "../components/SearchExercises";
 import Exercises from "../components/Exercises";
 
 const Home = () => {
+  const [exercises, setExercises] = useState("all");
+  const [bodyPart, setBodyPart] = useState([]);
+
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises />
-      <Exercises />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
     </Box>
   );
 };
